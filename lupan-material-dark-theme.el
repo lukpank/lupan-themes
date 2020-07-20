9487f51a590a0f96e635402e1d64d169cbe07373
Łukasz Pankowski <lukpank@o2.pl>
2020-07-20 20:17:43 +0200

    new dark and light (old renamed to material dark and material light)

---
+++ lupan-material-dark-theme.el
@@ -0,0 +1,7 @@
+(require 'lupan-themes)
+
+(deftheme lupan-material-dark "The dark variant of lupan's color theme (using material colors)")
+
+(lupan-themes-create-theme 'lupan-material-dark)
+
+(provide-theme 'lupan-material-dark)
---
+++ lupan-material-light-theme.el
@@ -0,0 +1,7 @@
+(require 'lupan-themes)
+
+(deftheme lupan-material-light "The light variant of lupan's color theme (using material colors)")
+
+(lupan-themes-create-theme 'lupan-material-light)
+
+(provide-theme 'lupan-material-light)
