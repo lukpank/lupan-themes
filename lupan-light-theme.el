3ec2ff5fa22f5bf7e0bba8b5974a71ad6a694b33
Łukasz Pankowski <lukpank@o2.pl>
2020-08-03 23:22:34 +0200

    light theme with relative colors, more like dark variants

--- lupan-light-theme.el
+++ lupan-light-theme.el
@@ -2,6 +2,6 @@
 
 (deftheme lupan-light "The light variant of lupan's color theme")
 
-(lupan-themes-create-light-theme 'lupan-light "#f2f6e1")
+(lupan-themes-create-light-theme 'lupan-light "#f2f6e1" 0.3 0.5)
 
 (provide-theme 'lupan-light)
